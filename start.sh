957af5c7ff4234a07b0ef6851913ab72d6149cf0
Robin Van den Broeck <robin.broeck@gmail.com>
2017-11-16 13:31:03 +0100

    Fixed problem with docker-compose not correctly shutting down the api

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ bundle exec bin/rails db:create
 bundle exec bin/rails db:migrate
 
 # Start the server
-bundle exec bin/rails s -p 3000 -b '0.0.0.0'
+exec bundle exec bin/rails s -p 3000 -b '0.0.0.0'
